30c2172502ac104b9321642915ddc122bdf05a7e
Alex <125412758+jenkaj-lab@users.noreply.github.com>
2025-01-19 20:47:58 +0000

    Create env_variables.py

---
+++ env_variables.py
@@ -0,0 +1,23 @@
+ABUSE_IPDB_API_KEY = (
+    # https://www.abuseipdb.com/
+    # 1,000 lookups per day
+    ""
+)
+
+IPDATA_API_KEY = (
+    # https://ipdata.co/
+    # 1,500 lookups per day
+    ""
+)
+
+IPINFO_ACCESS_TOKEN = (
+    # https://ipinfo.io/
+    # 50,000 lookups per month
+    ""
+)
+
+VPN_API_KEY = (
+    # https://vpnapi.io/
+    # 1,000 lookups per day
+    ""
+)
