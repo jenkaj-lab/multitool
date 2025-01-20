2d0cab7965c8e8fbdba75ea824147c1f2b6841ee
alex <jenkins.alexanderj@gmail.com>
2025-01-20 07:04:37 +0000

    removed ipv6

--- main.py
+++ main.py
@@ -44,12 +44,12 @@ class MultiTool:
     def check_for_ip_addresses(self, text):
 
         ipv4_regex = r"(?<!\d)(?:\d{1,3}\.){3}\d{1,3}(?!\d)"
-        ipv6_regex = r"((([0-9A-Fa-f]{1,4}:){1,6}:)|(([0-9A-Fa-f]{1,4}:){7}))([0-9A-Fa-f]{1,4})$"
+        ipv6_regex = ""
 
         ipv4_addresses = re.findall(ipv4_regex, text)
         ipv6_addresses = re.findall(ipv6_regex, text)
 
-        self.ip_addresses = ipv4_addresses + ipv6_addresses
+        self.ip_addresses = ipv4_addresses #+ ipv6_addresses
         self.highlight_ip_addresses()
 
     def highlight_ip_addresses(self):
