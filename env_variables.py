62bd99e781e040ce42b55a0e40ca4cfe6e1087a5
Alex <125412758+jenkaj-lab@users.noreply.github.com>
2025-03-01 11:39:14 +0000

    Update env_variables.py

--- env_variables.py
+++ env_variables.py
@@ -1,3 +1,9 @@
+IPINFO_ACCESS_TOKEN = (
+    # https://ipinfo.io/
+    # 50,000 lookups per month
+    ""
+)
+
 ABUSE_IPDB_API_KEY = (
     # https://www.abuseipdb.com/
     # 1,000 lookups per day
@@ -10,14 +16,20 @@ IPDATA_API_KEY = (
     ""
 )
 
-IPINFO_ACCESS_TOKEN = (
-    # https://ipinfo.io/
-    # 50,000 lookups per month
+VIRUSTOTAL_API_KEY = (
+    # https://www.virustotal.com/
+    # 4 lookups per minute, 500 lookups per day, 15k lookups per month
     ""
 )
 
 VPN_API_KEY = (
     # https://vpnapi.io/
+    # 1,000 lookups per day
+    ""
+)
+
+PROXYCHECK_API_KEY = (
+    # https://proxycheck.io/
     # 1,000 lookups per day
     ""
 )
